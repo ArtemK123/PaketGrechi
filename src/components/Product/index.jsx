2bccd805fcc834122ed22d44b8ec33ec57827069
Viktoriia Lizeikina <vlize@softserveinc.com>
2021-01-21 20:19:09 +0200

    update product styles

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -4,12 +4,17 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 function Product({src, name, store, link, price}) {
+    const [integer, fraction] = price.split('.');
     return (
         <div className='product'>
-          <img src={src} alt='product' />
-          <p>{name}</p>
-          <a href={link}>{store}</a>
-          <p className='product-price'>{price} ₴</p>
+            <img src={src} alt='product' />
+            <p className='name'>{name}</p>
+            <a href={link}>{store}</a>
+            <div className='price'>
+                <div className='integer'>{integer}</div>
+                <div className='fraction'>{fraction}</div>
+                <div className='currency'>₴</div>
+            </div>
         </div>
     )
 }
--- src/services/data.js
+++ src/services/data.js
@@ -6,7 +6,7 @@ function loadData() {
             store: 'Fora',
             src: 'https://content.fora.ua/sku/ecommerce/49/480x480wwm/490833_480x480wwm_88a02c98-62b1-64ee-92f6-716a89b6b396.png',
             link: 'https://shop.fora.ua/index.php?route=product/product&product_id=76297',
-            price: 37.99
+            price: '37.99'
         },
         {
             id: 2,
@@ -14,7 +14,7 @@ function loadData() {
             store: 'Fora',
             src: 'https://content.fora.ua/sku/ecommerce/48/480x480wwm/480943_480x480wwm_b87fbf98-edfe-1bc7-d8ba-b147c3d3a66f.png',
             link: 'https://shop.fora.ua/index.php?route=product/product&product_id=515952',
-            price: 41.57
+            price: '41.57'
         },
         {
             id: 3,
@@ -22,7 +22,7 @@ function loadData() {
             store: 'ATB',
             src: 'https://src.zakaz.atbmarket.com/cache/photos/catalog_product_main_6460.jpg',
             link: 'https://zakaz.atbmarket.com/product/531/82909',
-            price: 25.70
+            price: '25.70'
         },
         {
             id: 4,
@@ -30,7 +30,7 @@ function loadData() {
             store: 'Сільпо',
             src: 'https://content.silpo.ua/sku/ecommerce/69/480x480wwm/699717_480x480wwm_e61741c6-35be-4903-173e-f39fb658d2fa.png',
             link: 'https://www.atbmarket.com/',
-            price: 32.29
+            price: '32.29'
         }
     ]
 }
