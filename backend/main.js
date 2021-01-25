ed75147166aed039d3898871f109a13c45ef101c
ArtemK123 <darkmode1012@gmail.com>
2021-01-25 20:31:04 +0200

    Added CORS support in backend

--- backend/main.js
+++ backend/main.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
 app.listen(PORT, HOST);
