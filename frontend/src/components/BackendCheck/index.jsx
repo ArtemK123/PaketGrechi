f71f3527b3e50981953fd3041e79fe65858037fa
ArtemK123 <darkmode1012@gmail.com>
2021-01-26 09:03:04 +0200

    Moved backend connection check to console

--- frontend/src/components/BackendCheck/index.jsx
+++ frontend/src/components/BackendCheck/index.jsx
@@ -1,35 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import getEnvVariable from '../../services/getEnvVariable';
 
-import './style.css';
-
 const REFERRER_POLICY = 'no-referrer-when-downgrade';
+const SUCCESS_MESSAGE = 'Connection with backend was configured!';
+const ERROR_MESSAGE = 'Error while configuring connection with backend.';
 
 export default function BackendCheck() {
-    const [connectionConfigured, setConnectionConfigured] = useState(undefined);
-
     useEffect(() => {
-        if (connectionConfigured === undefined) {
-            fetch(getEnvVariable('REACT_APP_BACKEND_URL_BASE'), {
-                referrerPolicy: REFERRER_POLICY
-            })
-            .then(response => setConnectionConfigured(response.ok))
-            .catch(_ => setConnectionConfigured(false));
-        }
-    }, [connectionConfigured]);
-
-    if (connectionConfigured === true) {
-        return (
-            <p>
-                Connection with backend was configured!
-            </p>
-        )
-    }
-    else if (connectionConfigured === false) {
-        return (<p>
-            Error while configuring connection with backend
-        </p>);
-    }
+        fetch(getEnvVariable('REACT_APP_BACKEND_URL_BASE'), {
+            referrerPolicy: REFERRER_POLICY
+        })
+        .then(response => {
+            if (response.ok) {
+                console.log(SUCCESS_MESSAGE);
+            }
+            else {
+                console.log(`${ERROR_MESSAGE} Status code - ${response.status}`);
+            }
+        })
+        .catch(error => console.log(`${ERROR_MESSAGE} ${error}`));
+    }, []);
     return (null);
 }
